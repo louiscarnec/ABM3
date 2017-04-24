fbe2c932612e7f71b7ba3f60dfaf0d80a35623a8
louiscarnec <Carnec@Louiss-MacBook-Pro.local>
2017-04-24 18:03:26 +0100

    Delivery Louis idea

--- getCoordinates.py
+++ getCoordinates.py
@@ -13,7 +13,7 @@ import csv
 
 df = pd.DataFrame(columns=('City', 'Latitude', 'Longitude'))
 
-nodes = '/Users/Carnec/Desktop/Business_Analytics/analyticalbusinessmodelling/assignment3/coordinates5.csv'
+nodes = '/Users/Carnec/Desktop/Business_Analytics/analyticalbusinessmodelling/assignment3/coordinates20.csv'
 with open(nodes, 'r') as f:
     csvreader = csv.reader(f,delimiter = ',')
     i = 0
@@ -26,18 +26,28 @@ with open(nodes, 'r') as f:
         
 matrix = df.as_matrix() 
 dfdist = pd.DataFrame(columns=('City1', 'City2', 'Distance')) 
+step = 0
+for j in range(len(matrix)):
+    for k in range(len(matrix)):
+        dist =  vincenty((matrix[j][1],matrix[j][2]),(matrix[k][1],matrix[k][2])).km
+        if dist == float(0):
+            pass
+        else:
+            dfdist.loc[step]=[ matrix[j][0]), matrix[k][0], dist]
+            step +=1
+  
+np.savetxt(r'/Users/Carnec/Desktop/Business_Analytics/analyticalbusinessmodelling/assignment3/distbis.txt', dfdist,delimiter=" ", fmt="%s %s %10.3f")
 
 size = len(matrix)
 s = (size+1,size+1)
-distmatrix = np.zeros(s)
 i=0
-for i in range(size):
-    distmatrix[0][i] = str(matrix[i][0])
+#for i in range(size):
+#    distmatrix[0][i] = str(matrix[i][0])
 
 dfdist2 = pd.DataFrame(index=[df['City']],columns=[df['City']]) 
 for j in range(len(matrix)):
     for k in range(len(matrix)):
         dfdist2[matrix[j][0]][matrix[k][0]] = vincenty((matrix[j][1],matrix[j][2]),(matrix[k][1],matrix[k][2])).km
   
-np.savetxt(r'/Users/Carnec/Desktop/Business_Analytics/analyticalbusinessmodelling/assignment3/dist.txt', dfdist2.values, fmt='%d')
+#np.savetxt(r'/Users/Carnec/Desktop/Business_Analytics/analyticalbusinessmodelling/assignment3/dist.txt', dfdist2.values, fmt='%d')
 dfdist2.index    
